0a65405cf6962e5f32831c9350eea167a700ccaf
mayursaparia <mayursaparia@users.noreply.github.com>
2021-07-06 03:21:07 +0530

    Global Error Handling - API and Client side

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -10,7 +10,9 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+
         [Required]
+        [StringLength(8,MinimumLength =4)]
         public string Password { get; set; }
     }
 }
