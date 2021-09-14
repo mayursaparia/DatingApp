cd5b186cdb6f400a1f4ad4e5bd05a012de10bf19
mayursaparia <mayursaparia@users.noreply.github.com>
2021-09-14 20:00:41 +0530

    Commited after ready for production and before database change to postgres.

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-errors.component.css']
 })
 export class TestErrorsComponent implements OnInit {
-  baseUrl = "https://localhost:5001/api/";
+  baseUrl = environment.apiUrl;
   validationErrors: string[] = [];
 
   constructor(private http: HttpClient) { }
